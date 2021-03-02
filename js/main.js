afd3ccb30629268be0605a568cfcbbd07010e2c1
Amurdock6 <77793000+Amurdock6@users.noreply.github.com>
2021-03-02 10:13:10 -0700

    added svg

--- js/main.js
+++ js/main.js
@@ -3,5 +3,5 @@ window.addEventListener('load', () => {
 		const preload = document.querySelector('.preload');
 		setTimeout( function() {
             preload.classList.add('preload-finish');
-        }, 3000 );
+        }, 0 );
 });
