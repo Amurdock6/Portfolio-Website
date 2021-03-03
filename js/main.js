417f1e1e5dc1d9b59a361a99916e7f88a297c92f
Amurdock6 <77793000+Amurdock6@users.noreply.github.com>
2021-03-03 01:21:54 -0700

    added svg on work section and added on certficate click certficate takes up screen

--- js/main.js
+++ js/main.js
@@ -5,3 +5,25 @@ window.addEventListener('load', () => {
             preload.classList.add('preload-finish');
         }, 0 );
 });
+
+//image on click fill screen
+$('img[data-enlargeable]').addClass('img-enlargeable').click(function(){
+    var src = $(this).attr('src');
+    var modal;
+    function removeModal(){ modal.remove(); $('body').off('keyup.modal-close'); }
+    modal = $('<div>').css({
+        background: 'RGBA(0,0,0,.5) url('+src+') no-repeat center',
+        backgroundSize: 'contain',
+        width:'100%', height:'80%',
+        position:'fixed',
+        zIndex:'10000',
+        top:'10%', left:'0',
+        cursor: 'zoom-out'
+    }).click(function(){
+        removeModal();
+    }).appendTo('body');
+    //handling ESC
+    $('body').on('keyup.modal-close', function(e){
+      if(e.key==='Escape'){ removeModal(); } 
+    });
+});
